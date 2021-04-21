f28260f4cd32b7bfe89d0586a50bc9bbbee96078
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-04-21 16:51:20 -0700

    manager tests done

---
+++ __test__/Manager.test.js
@@ -0,0 +1,33 @@
+const Manager = require('../lib/manager.js');
+
+test('manager extends employee and adds office number', () => {
+  const manager = new Manager ("Pope", "Pius V", 1, "PopePiusV@vatican.com", 4)
+  expect(manager.firstName).toEqual(expect.any(String));
+  expect(manager.lastName).toEqual(expect.any(String));
+  expect(manager.id).toEqual(expect.any(Number));
+  expect(manager.email).toEqual(expect.any(String));
+  expect(manager.role).toBe('Manager');
+});
+
+test('given function getName', () => {
+  const manager = new Manager ("Pope", "Pius V", 1, "PopePiusV@vatican.com", 4)
+  expect(manager.firstName).toBe('Pope');
+  expect(manager.lastName).toBe('Pius V');
+});
+
+test('given function getId', () => {
+  const manager = new Manager ("Pope", "Pius V", 1, "PopePiusV@vatican.com", 4)
+  expect(manager.id).toEqual(expect.any(Number));
+});
+
+
+//expect.stringContaining in docs https://jestjs.io/docs/expect#expectstringcontainingstring
+test('given function getEmail', () => {
+  const manager = new Manager ("Pope", "Pius V", 1, "PopePiusV@vatican.com", 4)
+  expect(manager.email).toEqual(expect.stringContaining('@'));
+});
+
+test('given function getRole', () => {
+  const manager = new Manager ("Pope", "Pius V", 1, "PopePiusV@vatican.com", 4)
+  expect(manager.role).toBe('Manager');
+})
